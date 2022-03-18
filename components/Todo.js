4cb239976a789b0fca1521705412397f065140de
pakpongwanachompoo <s6301012620073@email.kmutnb.ac.th>
2022-03-19 00:41:01 +0700

    Add search_title,todo-list and Add_todo

--- components/Todo.js
+++ components/Todo.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { View,Text, StyleSheet } from 'react-native';
+import { View,Text, StyleSheet, } from 'react-native';
 
-const Task = () => {
+const Todo = (props) => {
 
     return(
-        <View>
-            <Text>Todo</Text>
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View style={styles.square}></View>
+                <Text style={styles.itemText}>{props.text}</Text>
+            </View>
+            <View style={styles.circular}></View>
         </View>
     )
 
 }
 
     const styles = StyleSheet.create({
-
+        item: {
+            backgroundColor: '#FFF',
+            paddingTop: 10,
+            paddingHorizontal: 15,
+            borderRadius: 10,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginBottom: 10,
+            borderColor: '#3DA9FC',
+        },
+        itemLeft: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            flexWrap: 'wrap'
+        },
+        square: {
+            width: 24,
+            height: 24,
+            backgroundColor: '#55BCF6',
+            opacity: 0.4,
+            borderRadius: 5,
+            marginRight: 15,
+        },
+        itemText: {
+            maxWidth: '80%',
+        },
+        circular: {
+            width: 12,
+            height: 12,
+            borderColor: '#55BCF6',
+            borderWidth: 2,
+            borderRadius: 5,
+        },
     });
 
-export default Task;
+export default Todo;
